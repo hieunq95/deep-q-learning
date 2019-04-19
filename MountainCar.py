312ca04850b55a02e14a99d848ced9afe514926c
Hieu Nguyen <hieu1204@gmail.com>
2019-04-19 14:40:17 +0800

    Update

--- MountainCar.py
+++ MountainCar.py
@@ -1,8 +1,11 @@
 # Mountain car using DQN
 
 import gym
+import numpy as np
 from collections import deque
 from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
 EPISODE = 1000
 
@@ -20,10 +23,32 @@ class CarAgent:
 
     def _build_model(self):
         model = Sequential()
+        model.add(Dense(24, input_dim=state_size, activation='relu'))
+        model.add(Dense(24, activation='relu'))
+        model.add(Dense(self.action_size, activation='linear'))
+        model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def remember(self, state, action, reward, nex_state, done):
+        self.memory.append((state, action, reward, nex_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return np.random.randn(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
+
+
 
 if __name__ == "__main__":
     env = gym.make('MountainCarContinuous-v0')
-    env.reset()
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.shape[0]
+    agent = CarAgent(state_size, action_size)
+    done = False
+    batch_size = 32
+
+    print(action_size, state_size)
     for i_episode in range(EPISODE):
         observation = env.reset()
         print(observation)
@@ -31,7 +56,7 @@ if __name__ == "__main__":
             env.render()
             action = env.action_space.sample()
             observation, reward, done, info = env.step(action)
-            obslist = list((observation, reward, done, info))
+            obslist = list((observation, action, reward, done, info))
             print(obslist)
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
